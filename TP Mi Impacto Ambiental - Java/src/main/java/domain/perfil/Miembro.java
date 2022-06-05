c9611ec76163592907860414671e8c7e9c535935
EDiazVaras <ediazvaras@frba.utn.edu.ar>
2022-06-05 20:04:51 -0300

    metodo faltante de miembro

--- TP Mi Impacto Ambiental - Java/src/main/java/domain/perfil/Miembro.java
+++ TP Mi Impacto Ambiental - Java/src/main/java/domain/perfil/Miembro.java
@@ -1,6 +1,7 @@
 package domain.perfil;
 
-import java.util.List;
+import domain.trayecto.Tramo;
+import domain.trayecto.Trayecto;
 
 public class Miembro {
     private String nombre;
@@ -9,8 +10,8 @@ public class Miembro {
     private Integer numeroDocumento;
     private List<Area> areas;
     private domain.perfil.Miembro Miembro; //TODO ver que onda con el Miembro
-    //TODO private list<Trayecto> trayectos;
-    //TODO private list<Tramos> tramosCompartidosAAceptar;
+    private List<Trayecto> trayectos;
+    private list<Tramos> tramosCompartidosAAceptar;
 
     public void darseAltaEnOrganizacion(Area area){
         area.agregarAMiembroPendiente(this.Miembro);
@@ -25,8 +26,19 @@ public class Miembro {
         return area.getOrganizacion(); //TODO consultar si es necesario que el miembro diga su organizacion, si lo evitamos mejoramos el encapsulamiento
     }
 
-    //public void gestionarTramosCompartidos(Tramo  tramosCompartido, Trayecto miTrayecto ){
-        //TODO gestionarTramosCompartidos
-    //}
+    public void recibirSolicitud(Tramo tramo){
+        this.tramosCompartidosAAceptar.add(tramo);
+    }
 
+    public void gestionarTramosCompartidos(Tramo tramoCompartido, Trayecto miTrayecto, boolean respuesta ){
+        this.tramosCompartidosAAceptar.remove(tramoCompartido);
+        if(respuesta) {
+            miTrayecto.agregarTramo(tramoCompartido);
+        }
+        //en caso de que sea rechazado no lo va a hacer nada
+    }
+
+    private Trayecto generarTrayecto(String descripcion){
+        trayectos.add(new Trayecto(descripcion));
+    }
 }
