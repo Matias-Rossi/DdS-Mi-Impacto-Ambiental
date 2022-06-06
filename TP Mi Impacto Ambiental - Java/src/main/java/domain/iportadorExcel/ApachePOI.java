da6ad679342e5d8f67bdc740839eacf6aed517f1
Pedro Torales <pedroimanoltorales@gmail.com>
2022-06-06 02:44:41 -0300

    Lector de excels anda

--- TP Mi Impacto Ambiental - Java/src/main/java/domain/iportadorExcel/ApachePOI.java
+++ TP Mi Impacto Ambiental - Java/src/main/java/domain/iportadorExcel/ApachePOI.java
@@ -1,6 +1,5 @@
 package domain.iportadorExcel;
 
-import config.Config;
 import java.util.*;
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -9,10 +8,13 @@ import java.util.Iterator;
 
 import org.apache.poi.ddf.NullEscherSerializationListener;
 import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellType;
 import org.apache.poi.ss.usermodel.DataFormatter;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import static domain.iportadorExcel.TipoActividad.LOGISTICA_DE_PRODUCTOS_Y_RESIDUOS;
 
 public class ApachePOI {
   //public static List <ActividadBase> listaDeFilas = new ArrayList<ActividadBase>();
@@ -20,7 +22,8 @@ public class ApachePOI {
 
   public static void main(String[] args){
     try{
-      FileInputStream f = new FileInputStream( Config.RUTA_IMPORTACION +"fechas.xlsx");
+
+      FileInputStream f = new FileInputStream( "C:/Users/pedro/OneDrive/Escritorio/file/fechas.xlsx");
       XSSFWorkbook libro = new XSSFWorkbook(f);
       XSSFSheet hoja = libro.getSheetAt(0);
       Iterator<Row> filas = hoja.iterator();
@@ -31,13 +34,13 @@ public class ApachePOI {
 
       TipoActividad valorTipoActividad;
       TipoConsumo valorTipoConsumo;
-      double valor;
+      double valor=0;
       TipoPeriodicidad valorTipoPeriodicidad;
       String valorPeriodoDeImputacion;
-      TipoProductoTransportado valorTipoProductoTransportado;
-      TipoTransporteUtilizado valorTipoTransporteUtilizado;
-      double valorDistanciaMedia;
-      double valorPesoTotal;
+      TipoProductoTransportado valorTipoProductoTransportado=null;
+      TipoTransporteUtilizado valorTipoTransporteUtilizado=null;
+      double valorDistanciaMedia=0;
+      double valorPesoTotal=0;
       CargaSegunActividad cargaSegunActividad;
       List<CargaSegunActividad> listaDeCargas=new ArrayList<CargaSegunActividad>();
 
@@ -56,58 +59,61 @@ public class ApachePOI {
         celda = celdas.next();
         valorTipoConsumo = TipoConsumo.valueOf(celda.getStringCellValue());
 
-        switch (valorTipoActividad){
-          case LOGISTICA_DE_PRODUCTOS_Y_RESIDUOS: {
-
-            for (int n=0;n<4;n++){
-              celda = celdas.next();
-              switch (valorTipoConsumo){
-                case PRODUCTO_TRANSPORTADO:valorTipoProductoTransportado=TipoProductoTransportado.valueOf(celda.getStringCellValue());
-                case MEDIO_DE_TRANSPORTE:valorTipoTransporteUtilizado=TipoTransporteUtilizado.valueOf(celda.getStringCellValue());
-                case DISTANCIA_MEDIA:valorDistanciaMedia=celda.getNumericCellValue();
-                case PESO_TOTAL:valorPesoTotal=celda.getNumericCellValue();
+        if(valorTipoActividad==LOGISTICA_DE_PRODUCTOS_Y_RESIDUOS){
+          for (int n=0;n<4;n++){
+            celda = celdas.next();
+            switch (valorTipoConsumo){
+              case PRODUCTO_TRANSPORTADO:{
+                valorTipoProductoTransportado=TipoProductoTransportado.valueOf(celda.getStringCellValue());
+                break;
               }
+              case MEDIO_DE_TRANSPORTE:{
+                valorTipoTransporteUtilizado=TipoTransporteUtilizado.valueOf(celda.getStringCellValue());
+                break;
+              }
+              case DISTANCIA_MEDIA:{
+                valorDistanciaMedia=celda.getNumericCellValue();
+                break;
+              }
+              case PESO_TOTAL:{
+                valorPesoTotal=celda.getNumericCellValue();
+                break;
+              }
+              default: {
+                break;
+              }
+            }
+            if(n<3){
               fila=filas.next();
               celdas = fila.cellIterator();
               celda = celdas.next();
               celda = celdas.next();
               valorTipoConsumo = TipoConsumo.valueOf(celda.getStringCellValue());
-
             }
-
           }
-          default:{
-            celda = celdas.next();
-            valor = celda.getNumericCellValue();
-            valorTipoProductoTransportado = null;
-            valorTipoTransporteUtilizado = null;
-            valorDistanciaMedia = 0;
-            valorPesoTotal = 0;
-          }
+        }else{
+          celda = celdas.next();
+          valor = celda.getNumericCellValue();
+          valorTipoProductoTransportado = null;
+          valorTipoTransporteUtilizado = null;
+          valorDistanciaMedia = 0;
+          valorPesoTotal = 0;
         }
+
+
         celda = celdas.next();
         valorTipoPeriodicidad = TipoPeriodicidad.valueOf(celda.getStringCellValue());
 
         celda = celdas.next();
         valorPeriodoDeImputacion = formatter.formatCellValue(celda);
 
-        switch (valorTipoActividad){
-          case LOGISTICA_DE_PRODUCTOS_Y_RESIDUOS: cargaSegunActividad=new ActividadLogistica(valorTipoActividad, valorTipoProductoTransportado, valorTipoTransporteUtilizado, valorDistanciaMedia, valorPesoTotal, valorTipoPeriodicidad, valorPeriodoDeImputacion);
-          default:cargaSegunActividad=new ActividadGenerica(valorTipoActividad,valorTipoConsumo,valor,valorTipoPeriodicidad,valorPeriodoDeImputacion);
-        }
 
-        System.out.println(valorTipoActividad);
-        System.out.println(valorTipoConsumo);
-        System.out.println(valor);
-        System.out.println(valorTipoPeriodicidad);
-        System.out.println(valorPeriodoDeImputacion);
-        System.out.println(valorTipoProductoTransportado);
-        System.out.println(valorTipoTransporteUtilizado);
-        System.out.println(valorDistanciaMedia);
-        System.out.println(valorPesoTotal);
+        if(valorTipoActividad==LOGISTICA_DE_PRODUCTOS_Y_RESIDUOS)
+          cargaSegunActividad=new ActividadLogistica(valorTipoActividad, valorTipoProductoTransportado, valorTipoTransporteUtilizado, valorDistanciaMedia, valorPesoTotal, valorTipoPeriodicidad, valorPeriodoDeImputacion);
+          else
+            cargaSegunActividad=new ActividadGenerica(valorTipoActividad,valorTipoConsumo,valor,valorTipoPeriodicidad,valorPeriodoDeImputacion);
 
         listaDeCargas.add(cargaSegunActividad);
-
       }
       f.close();
     } catch (
