779ad2b3af67b80f1051f8037e26813006e283a3
ITantucci <itantucci@frba.utn.edu.ar>
2022-10-17 09:41:38 -0300

    aa

--- DiseñoWeb/javascript.js
+++ DiseñoWeb/javascript.js
@@ -102,6 +102,178 @@ function checkPassword(data) {
   else { requisitosCase.classList.remove('valid'); }
 }
 
+function setPasswordConfirmValidityOrg(str) {
+  var password1 = document.getElementById('inputContrseñaOrg');
+  var password2 = document.getElementById('inputConfirmarContrseñaOrg');
+  var contrseñaNoCoincide = document.getElementById('contrseñaNoCoincideOrg');
+  var contrseñaCoincide = document.getElementById('contrseñaCoincideOrg');
+
+  if (password1.value === password2.value) {
+    contrseñaCoincide.classList.add('valid');
+    contrseñaNoCoincide.classList.add('d-none');
+    contrseñaCoincide.classList.remove('d-none');
+  } else {
+    contrseñaCoincide.classList.remove('valid');
+    contrseñaNoCoincide.classList.remove('d-none');
+    contrseñaCoincide.classList.add('d-none');
+  }
+}
+function togglePasswordF12() {
+  var togglePassword12 = document.getElementById('togglePassword12');
+  var password1 = document.getElementById('inputContrseñaOrg');
+
+  if (password1.type === 'password') {
+    password12.setAttribute('type', 'text');
+    togglePassword12.classList.add('hide');
+  } else {
+    password12.setAttribute('type', 'password');
+    togglePassword12.classList.remove('hide');
+  }
+}
+
+function togglePasswordF22() {
+  var togglePassword22 = document.getElementById('togglePassword22');
+  var password22 = document.getElementById('inputConfirmarContrseñaOrg');
+
+  if (password22.type === 'password') {
+    password22.setAttribute('type', 'text');
+    togglePassword22.classList.add('hide');
+  } else {
+    password22.setAttribute('type', 'password');
+    togglePassword22.classList.remove('hide');
+  }
+}
+
+function checkPasswordOrg(data) {
+
+  var lowerCase = document.getElementById('lowerOrg');
+  var upperCase = document.getElementById('upperOrg');
+  var numberCase = document.getElementById('numberOrg');
+  var specialCase = document.getElementById('specialOrg');
+  var lengthCase = document.getElementById('lengthOrg');
+  var requisitosCase = document.getElementById('requisitosOrg');
+
+  const lower = new RegExp('(?=.*[a-z])');
+  const upper = new RegExp('(?=.*[A-Z])');
+  const special = new RegExp('(?=.*[!@#\$%\^&\*_=+-])');
+  const number = new RegExp('(?=.*[0-9])');
+  const length = new RegExp('.{8,32}$');
+
+  //lower case Validation check
+  if (lower.test(data)) { lowerCase.classList.add('valid'); }
+  else { lowerCase.classList.remove('valid'); }
+
+  //upper case Validation check
+  if (upper.test(data)) { upperCase.classList.add('valid'); }
+  else { upperCase.classList.remove('valid'); }
+
+  //number Validation check
+  if (number.test(data)) { numberCase.classList.add('valid'); }
+  else { numberCase.classList.remove('valid'); }
+
+  //special character Validation check
+  if (special.test(data)) { specialCase.classList.add('valid'); }
+  else { specialCase.classList.remove('valid'); }
+
+  //length Validation check
+  if (length.test(data)) { lengthCase.classList.add('valid'); }
+  else { lengthCase.classList.remove('valid'); }
+
+  if (length.test(data) && special.test(data) && number.test(data) && upper.test(data) && lower.test(data)) { requisitosCase.classList.add('valid'); }
+  else { requisitosCase.classList.remove('valid'); }
+}
+
+
+function setPasswordConfirmValidityAS(str) {
+  var password1 = document.getElementById('inputContrseñaAS');
+  var password2 = document.getElementById('inputConfirmarContrseñaAS');
+  var contrseñaNoCoincide = document.getElementById('contrseñaNoCoincideAS');
+  var contrseñaCoincide = document.getElementById('contrseñaCoincideAS');
+
+  if (password1.value === password2.value) {
+    contrseñaCoincide.classList.add('valid');
+    contrseñaNoCoincide.classList.add('d-none');
+    contrseñaCoincide.classList.remove('d-none');
+  } else {
+    contrseñaCoincide.classList.remove('valid');
+    contrseñaNoCoincide.classList.remove('d-none');
+    contrseñaCoincide.classList.add('d-none');
+  }
+}
+function togglePasswordF13() {
+  var togglePassword1 = document.getElementById('togglePassword13');
+  var password1 = document.getElementById('inputContrseñaAS');
+
+  if (password1.type === 'password') {
+    password1.setAttribute('type', 'text');
+    togglePassword1.classList.add('hide');
+  } else {
+    password1.setAttribute('type', 'password');
+    togglePassword1.classList.remove('hide');
+  }
+}
+
+function togglePasswordF23() {
+  var togglePassword2 = document.getElementById('togglePassword23');
+  var password2 = document.getElementById('inputConfirmarContrseñaAS');
+
+  if (password2.type === 'password') {
+    password2.setAttribute('type', 'text');
+    togglePassword2.classList.add('hide');
+  } else {
+    password2.setAttribute('type', 'password');
+    togglePassword2.classList.remove('hide');
+  }
+}
+
+function checkPasswordAS(data) {
+
+  var lowerCase = document.getElementById('lowerAS');
+  var upperCase = document.getElementById('upperAS');
+  var numberCase = document.getElementById('numberAS');
+  var specialCase = document.getElementById('specialAS');
+  var lengthCase = document.getElementById('lengthAS');
+  var requisitosCase = document.getElementById('requisitosAS');
+
+  const lower = new RegExp('(?=.*[a-z])');
+  const upper = new RegExp('(?=.*[A-Z])');
+  const special = new RegExp('(?=.*[!@#\$%\^&\*_=+-])');
+  const number = new RegExp('(?=.*[0-9])');
+  const length = new RegExp('.{8,32}$');
+
+  //lower case Validation check
+  if (lower.test(data)) { lowerCase.classList.add('valid'); }
+  else { lowerCase.classList.remove('valid'); }
+
+  //upper case Validation check
+  if (upper.test(data)) { upperCase.classList.add('valid'); }
+  else { upperCase.classList.remove('valid'); }
+
+  //number Validation check
+  if (number.test(data)) { numberCase.classList.add('valid'); }
+  else { numberCase.classList.remove('valid'); }
+
+  //special character Validation check
+  if (special.test(data)) { specialCase.classList.add('valid'); }
+  else { specialCase.classList.remove('valid'); }
+
+  //length Validation check
+  if (length.test(data)) { lengthCase.classList.add('valid'); }
+  else { lengthCase.classList.remove('valid'); }
+
+  if (length.test(data) && special.test(data) && number.test(data) && upper.test(data) && lower.test(data)) { requisitosCase.classList.add('valid'); }
+  else { requisitosCase.classList.remove('valid'); }
+}
+
+
+
+
+
+
+
+
+
+
 var mostrarValidaciones = function (id) {
   var current = document.getElementById(id);
   current.classList.add('valid-feedback');
@@ -192,43 +364,7 @@ function grabOrgArea() {
 }
 
 
-window.onload = function mostrarLineas() {
-  //Get html elements
-  var tipoDeTransportePublico = document.getElementById("tipoDeTransportePublico");
-  var linea = document.getElementById("linea");
-  var paradaInicio = document.getElementById("paradaInicio");
 
-  //Load tipoDeTransporte
-  for (var tipoDeTransporte in tipoDeTransportePublicoInfo) {
-    tipoDeTransportePublico.options[tipoDeTransportePublico.options.length] = new Option(tipoDeTransporte, tipoDeTransporte);
-  }
-  //tipoDeTransporte Changed
-  tipoDeTransportePublico.onchange = function () {
-    linea.length = 1; // remove all options bar first
-    if (this.selectedIndex < 1)
-      return; // done
-
-    for (var lineaDeTransporte in tipoDeTransportePublicoInfo[this.value]) {
-      linea.options[linea.options.length] = new Option(lineaDeTransporte, tipoDeTransportePublicoInfo[this.value][lineaDeTransporte]);
-
-    }
-    }
-
-    linea.onchange = function () {
-      var lineaInfo = tipoDeTransportePublicoInfo[this.value];
-      paradaInicio.length = 1; // remove all options bar first
-      if (this.selectedIndex < 1)
-        return; // done
-  
-        for (var paradaInicioTransporte in lineaInfo[this.value]) {
-          paradaInicio.options[paradaInicio.options.length] = new Option(paradaInicioTransporte, lineaInfo[this.value][paradaInicioTransporte]);
-        }
-      }
-
-
-
-
-  }
 
 
 
