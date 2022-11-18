f06617d63f31b9846a4eda239c95dd2b9d7ff981
tsoiffer <tsoiffer@frba.utn.edu.ar>
2022-11-18 14:01:36 -0300

    cambios para el deploy

--- src/test/java/domain/calculadorHC/TestCalculadorHC.java
+++ src/test/java/domain/calculadorHC/TestCalculadorHC.java
@@ -1,7 +1,7 @@
 package domain.calculadorHC;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+//import static org.junit.jupiter.api.Assertions.assertEquals;
+//import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class TestCalculadorHC {
     /*
